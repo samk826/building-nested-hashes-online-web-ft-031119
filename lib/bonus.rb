1f5b456f7bc0e96d2c20905b27b836ac7edad10d
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-17 23:12:25 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,10 +20,11 @@ def bonus
    }
   }
 
+
+  #code your solution here:
   epic_tragedy[:capulet][:heroine][:status]="dead"
   epic_tragedy[:montague][:hero][:status]="dead"
 end
-  #code your solution here:
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash	  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
     epic_tragedy
